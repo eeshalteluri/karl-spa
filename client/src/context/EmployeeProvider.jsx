a617d865eed98d686b84994afb23a36a28b48b60
eeshalteluri <eeshalteluri@gmail.com>
2024-10-15 21:37:30 +0530

    adding localhost to access backend server.

--- client/src/context/EmployeeProvider.jsx
+++ client/src/context/EmployeeProvider.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 
+import toast, { Toaster } from 'react-hot-toast'
+
 import { BACKEND_URL } from '../../config'
 
 export const EmployeeContext = createContext()
@@ -17,16 +19,19 @@ export const EmployeeProvider = ({children}) => {
     const addEmployee = async (newEmployee) => {
         await axios.post(`${BACKEND_URL}`, newEmployee)
         await getEmployees()
+        toast.success('Employee added successfully')
     }
 
     const removeEmployee = async (id) => {
         await axios.delete(`${BACKEND_URL}/${id}`)
         await getEmployees()
+        toast.success('Employee removed successfully')
     }
 
     const updateEmployee = async (id, updatedEmployee) => {
         await axios.put(`${BACKEND_URL}/${id}`, updatedEmployee)
         await getEmployees()
+        toast.success('Employee updated successfully')
     }
 
     useEffect(() => {
@@ -36,6 +41,7 @@ export const EmployeeProvider = ({children}) => {
     return (
         <EmployeeContext.Provider value={{ employees, addEmployee, removeEmployee, updateEmployee }}>
             {children}
+            <Toaster />
         </EmployeeContext.Provider>
     )
 }
